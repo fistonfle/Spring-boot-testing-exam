c883acd26cb9ce0ace4a96758deec42046266364
Fiston Nshimiyandinze <fistonfle04@gmail.com>
2021-12-16 16:11:28 +0200

    ft(testing) : city service tests

---
+++ src/main/java/rw/ac/rca/termOneExam/service/CityService.java
@@ -0,0 +1,54 @@
+package rw.ac.rca.termOneExam.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.dto.CreateCityDTO;
+import rw.ac.rca.termOneExam.repository.ICityRepository;
+
+@Service
+public class CityService {
+
+	@Autowired
+	private ICityRepository cityRepository;
+
+	public Optional<City> getById(long id) {
+
+		Optional<City> cityOptional = cityRepository.findById(id);
+		cityOptional.get().setFahrenheit((cityOptional.get().getWeather() * 9/5) + 32);
+
+		return cityOptional;
+	}
+
+	public List<City> getAll() {
+
+		List<City> cities = new ArrayList<>();
+
+		for(City city: cityRepository.findAll()) {
+
+			city.setFahrenheit( (city.getWeather() * 9/5) + 32);
+			cities.add(city);
+
+		}
+
+		return cities;
+	}
+
+	public boolean existsByName(String name) {
+
+		return cityRepository.existsByName(name);
+	}
+
+	public City save(CreateCityDTO dto) {
+		City city =  new City(dto.getName(), dto.getWeather());
+		city.setFahrenheit( (dto.getWeather() * 9/5) + 32);
+		return cityRepository.save(city);
+	}
+
+
+}
---
+++ src/test/java/rw/ac/rca/termOneExam/controller/CityControllerIntegrationTest.java
@@ -0,0 +1,90 @@
+/**
+ * @author: fiston nshimiyandinze
+ * @description: city controller integration testing
+ */
+
+package rw.ac.rca.termOneExam.controller;
+
+import org.json.JSONException;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import rw.ac.rca.termOneExam.domain.City;
+import rw.ac.rca.termOneExam.dto.CreateCityDTO;
+import rw.ac.rca.termOneExam.utils.APICustomResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CityControllerIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getAll_success() throws JSONException {
+        String response = this.restTemplate.getForObject("/api/cities/all", String.class);
+        System.out.println(response);
+        JSONAssert.assertEquals("[{\"id\":101,\"name\":\"Kigali\",\"weather\":24.0,\"fahrenheit\":75.2},{\"id\":102,\"name\":\"Musanze\",\"weather\":18.0,\"fahrenheit\":64.4},{\"id\":103,\"name\":\"Rubavu\",\"weather\":20.0,\"fahrenheit\":68.0},{\"id\":104,\"name\":\"Nyagatare\",\"weather\":28.0,\"fahrenheit\":82.4}]", response, true);
+    }
+
+    @Test
+    public void getById_success() {
+
+        ResponseEntity<City> response = this.restTemplate.getForEntity("/api/cities/id/101", City.class);
+
+        System.out.println(response);
+        assertEquals(200, response.getStatusCodeValue());
+        assertEquals(24.0, response.getBody().getWeather());
+        assertEquals(75.2, response.getBody().getFahrenheit());
+        assertEquals("Kigali", response.getBody().getName());
+    }
+
+    @Test
+    public void getById_failed() {
+
+        ResponseEntity<APICustomResponse> response = this.restTemplate.getForEntity("/api/cities/id/340", APICustomResponse.class);
+
+        System.out.println("...."+response);
+        assertEquals(404, response.getStatusCodeValue());
+        assertEquals("City not found with id 340",response.getBody().getMessage());
+    }
+
+    @Test
+    public void createItem_Success() {
+
+        CreateCityDTO cityDTO=new CreateCityDTO();
+
+        cityDTO.setName("Nyabihu");
+        cityDTO.setWeather(0);
+
+        ResponseEntity<City> response = this.restTemplate.postForEntity("/api/cities/add", cityDTO, City.class);
+
+        assertEquals(201, response.getStatusCodeValue());
+        assertEquals("Nyabihu",response.getBody().getName());
+        assertEquals(32,response.getBody().getFahrenheit());
+    }
+
+    @Test
+    public void createItem_BAD_REQUEST() {
+
+        CreateCityDTO cityDTO=new CreateCityDTO();
+
+        cityDTO.setName("Kigali");
+
+        cityDTO.setWeather(19.0);
+
+        ResponseEntity<City> response = this.restTemplate.postForEntity("/api/cities/add", cityDTO, City.class);
+
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+
+}
